174e197ca8129031fbe6f4d4a284768dbeb99a92
Immanuel Bayalkoti <bayalkotiimmanuel@gmail.com>
2024-02-11 00:25:55 +0545

    change on form

--- userside/app/hire/page.tsx
+++ userside/app/hire/page.tsx
@@ -39,26 +39,23 @@ const Hire: NextPage = (): ReactElement => {
 
                 <div className="form flex-1 flex flex-col flex-wrap items-center gap-3">
                     <div className="flex gap-2">
-                        <div className="w-[300px]">
-                            <label htmlFor="underline_select" className="sr-only">Model</label>
-                            <select id="underline_select" className="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer">
-                                <option>Choose a Model</option>
-                                <option selected value="US">Aayusha Pokherel</option>
-                                <option value="CA">Aayusha Pokherel</option>
-                                <option value="FR">Aayusha Pokherel</option>
-                                <option value="DE">Aayusha Pokherel</option>
-                            </select>
-                        </div>
+                        <select id="models" className="w-full min-w-[20vw] text-gray-500 bg-transparent border border-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            <option>Choose a country</option>
+                            <option selected value="US">Aayusha Poudel</option>
+                            <option value="Aayusha Poudel">Aayusha Poudel</option>
+                            <option value="Aayusha Poudel">Aayusha Poudel</option>
+                            <option value="Aayusha Poudel">Aayusha Poudel</option>
+                        </select>
                         <Input type="text" placeholder="Subject"></Input>
                     </div>
-                    <div className="flex gap-2">
-                        <div className="relative w-[300px]">
+                    <div className="flex gap-2 items-center">
+                        <div className="relative w-[300px] min-w-[20vw]">
                             <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
                                 <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                 <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
                                 </svg>
                             </div>
-                            <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date" />
+                            <input type="text" className="bg-transparent border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date" />
                         </div>
                         <Input type="text" placeholder="Phone"></Input>
                     </div>
